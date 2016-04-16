28142cb058d7564a679027b5a63b352a626344d0
“Klammertime” <audrey.ck@gmail.com>
2016-04-16 10:07:49 -0700

    chore: change README.md, update Gulp

---
+++ dist/js/resumeBuilder.js
@@ -0,0 +1,180 @@
+var work = {
+    "jobs": [{
+        "employer": "Employer",
+        "title": "Front-end Developer",
+        "location": "SF Bay Area",
+        "dates": "2009",
+        "description": "Stuff about my job."
+    }, {
+        "employer": "Employer",
+        "title": "Front-end Developer",
+        "location": "SF Bay Area",
+        "dates": "2009",
+        "description": "Stuff about my job."
+    }]
+};
+
+var projects = {
+    "projects": [{
+        "title": "Random1",
+        "dates": "2015",
+        "description": "Description for my project.",
+        "images": ["img/portfolio1_full.jpg", "img/portfolio1_thumb.jpg"]
+    }, {
+        "title": "Random2",
+        "dates": "2014",
+        "description": "Description for my project.",
+        "images": ["img/portfolio2_full.jpg", "img/portfolio2_thumb.jpg"]
+    }, {
+        "title": "Random3",
+        "dates": "2013",
+        "description": "Description for my project.",
+        "images": ["img/portfolio3_full.jpg", "img/portfolio3_thumb.jpg"]
+    }]
+};
+
+var education = {
+    "schools": [{
+        "name": "UCSB",
+        "location": "SB",
+        "degree": "BA",
+        "majors": ["Communication"],
+        "dates": "2001-2001",
+        "url": "college.com"
+    }, {
+        "name": "UCSB",
+        "location": "SB",
+        "degree": "BA",
+        "majors": ["Communication"],
+        "dates": "2001-2001",
+        "url": "college.com"
+    }],
+    "onlineCourses": [{
+        "title": "JS",
+        "school": "Udacity",
+        "date": "3/2015",
+        "url": "myonlinecourse.com"
+    }]
+};
+
+var bio = {
+    "name": "Audrey Klammer",
+    "role": "Front-end Developer",
+    "contacts": {
+        "mobile": "206-962-0792",
+        "email": "audrey@gmail.com",
+        "github": "klammertime",
+        "twitter": "@klammertime",
+        "location": "SF Bay Area"
+    },
+    "welcomeMessage": "This is my welcome message.",
+    "skills": [
+        "JavaScript", "HTML5", "jQuery", "Responsive Web Design"
+    ],
+    "biopic": "img/audreyKlammer_full.jpg"
+};
+
+bio.display = function() {
+    var obj, nameTitle, myContact, picWelcome, mySkill = "";
+    nameTitle = HTMLheaderName.replace("%data%", bio.name) +
+        HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(nameTitle);
+    obj = bio.contacts;
+    myContact = HTMLmobile.replace("%data%", obj.mobile);
+    myContact += HTMLemail.replace("%data%", obj.email);
+    myContact += HTMLtwitter.replace("%data%", obj.twitter);
+    myContact += HTMLgithub.replace("%data%", obj.github);
+    myContact += HTMLlocation.replace("%data%", obj.location);
+    $("#topContacts").append(myContact);
+    $("#footerContacts").append(myContact);
+    picWelcome = HTMLbioPic.replace("%data%", bio.biopic) +
+        HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(picWelcome);
+    if (bio.skills.length) {
+        $("#header").append(HTMLskillsStart);
+        for (var i = 0; i < bio.skills.length; i++) {
+            mySkill += HTMLskills.replace("%data%", bio.skills[i]);
+        }
+        $("#skills").append(mySkill);
+    }
+};
+
+education.display = function() {
+    var obj, myDegree, myCourse;
+    for (var school in education.schools) {
+        obj = education.schools;
+        if (obj.hasOwnProperty(school)) {
+            $("#education").append(HTMLschoolStart);
+            myDegree = HTMLschoolName.replace("%data%", obj[school].name);
+            myDegree += HTMLschoolDegree.replace("%data%", obj[school].degree);
+            myDegree += HTMLschoolDates.replace("%data%", obj[school].dates);
+            myDegree += HTMLschoolLocation.replace("%data%", obj[school].location);
+            myDegree += HTMLschoolMajor.replace("%data%", obj[school].majors[0]);
+            $(".education-entry:last").append(myDegree);
+        }
+    }
+
+    for (school in education.onlineCourses) {
+        obj = education.onlineCourses;
+        if (obj.hasOwnProperty(school)) {
+            myCourse = HTMLonlineClasses + HTMLonlineTitle.replace("%data%", obj[school].title);
+            myCourse += HTMLonlineSchool.replace("%data%", obj[school].school);
+            myCourse += HTMLonlineDates.replace("%data%", obj[school].date);
+            myCourse += HTMLonlineURL.replace("%data%", obj[school].url);
+            $(".education-entry:last").append(myCourse);
+        }
+    }
+};
+
+work.display = function() {
+    var obj, myJob;
+    for (var job in work.jobs) {
+        obj = work.jobs;
+        if (obj.hasOwnProperty(job)) {
+            $("#workExperience").append(HTMLworkStart);
+            myJob = HTMLworkEmployer.replace("%data%", obj[job].employer);
+            myJob += HTMLworkTitle.replace("%data%", obj[job].title);
+            myJob += HTMLworkDates.replace("%data%", obj[job].dates);
+            myJob += HTMLworkLocation.replace("%data%", obj[job].location);
+            myJob += HTMLworkDescription.replace("%data%", obj[job].description);
+            $(".work-entry:last").append(myJob);
+        }
+    }
+};
+
+projects.display = function() {
+    var obj, myProject;
+    $("#projects").append(HTMLimageViewerStart);
+    $("#photo-viewer").after(HTMLimageViewerContainer);
+    /* photoviewer inserted here. Adapted from book "JavaScript & jQuery" by Jon Duckett */
+    for (var project in projects.projects) {
+        obj = projects.projects;
+        if (obj.hasOwnProperty(project)) {
+            myProject = HTMLimageLink.replace("%data%", obj[project].images[0]);
+            myProject += HTMLimageTitle.replace("%data%", obj[project].title);
+            myProject += HTMLimageThumb.replace("%data%", obj[project].images[1]);
+            myProject += HTMLimageThumbAlt.replace("%data%", obj[project].title);
+            $("#thumbnails").prepend(myProject);
+        }
+    }
+    for (project in projects.projects) {
+        obj = projects.projects;
+        if (obj.hasOwnProperty(project)) {
+            $("#projects").append(HTMLprojectStart);
+            myProject = HTMLprojectTitle.replace("%data%", obj[project].title);
+            myProject += HTMLprojectDates.replace("%data%", obj[project].dates);
+            myProject += HTMLprojectDescription.replace("%data%", obj[project].description);
+            $(".project-entry:last").append(myProject);
+        }
+    }
+};
+
+
+work.display();
+projects.display();
+education.display();
+bio.display();
+
+$("#mapDiv").append(googleMap);
+
+// $("#main").append(internationalizeButton);
--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,96 @@
 'use strict';
 var gulp = require('gulp'),
-  cssmin = require('gulp-cssmin'),
-  rename = require('gulp-rename'),
-     del = require('del'),
-imagemin = require('gulp-imagemin'),
-imageResize = require('gulp-image-resize'),
-pngquant = require('imagemin-pngquant'),
-imageminJpegRecompress = require('imagemin-jpeg-recompress');
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    imageResize = require('gulp-image-resize'),
+    pngquant = require('imagemin-pngquant'),
+    pages = require('gulp-gh-pages'),
 
-gulp.task('minifyCSS', function(){
-  gulp.src("css/style.css")
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest("css"));
+    imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
+var options = {
+    dist: './dist/'
+};
+
+gulp.task('minifyCSS', function() {
+    gulp.src("css/style.css")
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("css"));
 });
-
 
 /* Want these image widths: 600px, 960px, 1440px, 1800px, 3600px
   highest average original size for all */
 
-gulp.task('resizeFull', function () {
-  return gulp.src('img_original/*')
-    .pipe(imageResize({
-      height: 450,
-      upscale : false,
-      imageMagick: true
-    }))
-    .pipe(rename({suffix: '_full'}))
-    .pipe(gulp.dest('img'));
+gulp.task('resizeFull', function() {
+    return gulp.src('img_original/*')
+        .pipe(imageResize({
+            height: 450,
+            upscale: false,
+            imageMagick: true
+        }))
+        .pipe(rename({ suffix: '_full' }))
+        .pipe(gulp.dest('img'));
 });
 
-gulp.task('resizeThumb', function () {
-  return gulp.src('img_original/*')
-    .pipe(imageResize({
-      width : 215,
-      upscale : false,
-      imageMagick: true
-    }))
-    .pipe(rename({suffix: '_thumb'}))
-    .pipe(gulp.dest('img'));
+gulp.task('resizeThumb', function() {
+    return gulp.src('img_original/*')
+        .pipe(imageResize({
+            width: 215,
+            upscale: false,
+            imageMagick: true
+        }))
+        .pipe(rename({ suffix: '_thumb' }))
+        .pipe(gulp.dest('img'));
 });
 
 
 // After images resized need to be optimized or compressed using an algorithm that decides
 // what data to keep and what it can throw away while still maintaining visual integrity.
 // Plus need to get rid of extra metadata added during imageResize.
-gulp.task('optimize', ['resizeFull', 'resizeThumb'], function () {
-  return gulp.src('img/*')
-    .pipe(imagemin({
-      use:[imageminJpegRecompress({
-        loops:4,
-        min: 60,
-        max: 95,
-        quality:'high'
-      })]
-    }))
-    .pipe(gulp.dest('img'));
+gulp.task('optimize', ['resizeFull', 'resizeThumb'], function() {
+    return gulp.src('img/*')
+        .pipe(imagemin({
+            use: [imageminJpegRecompress({
+                loops: 4,
+                min: 60,
+                max: 95,
+                quality: 'high'
+            })]
+        }))
+        .pipe(gulp.dest('img'));
 });
 
-gulp.task('watchFiles', function(){
-  gulp.watch('img_original/*', ['resizeFull', 'resizeThumb']);
+gulp.task('watchFiles', function() {
+    gulp.watch('img_original/*', ['resizeFull', 'resizeThumb']);
 });
 
 // creating clean task
-gulp.task('clean', function(){
-  del(['dist', 'img', 'css/style.min.css']);
+gulp.task('clean', function() {
+    del(['dist', 'img', 'css/style.min.css']);
 });
 
-gulp.task("build", ['optimize', 'minifyCSS'], function(){
-  return gulp.src([
-    "css/style.min.css",
-    "index.html",
-    "img/**"], { base: "./"})
-      .pipe(gulp.dest("dist"));
+gulp.task('deploy', function() {
+    return gulp.src(options.dist + '**/*')
+        .pipe(pages());
+})
+
+gulp.task("build", ['optimize', 'minifyCSS'], function() {
+    return gulp.src([
+            "css/style.min.css",
+            "index.html",
+            "img/**",
+            "js/**"
+        ], { base: "./" })
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task('serve', ['watchFiles']);
 
 // we make build task a depend. of default task so
 // can just run gulp
-gulp.task("default", ["clean"], function(){
-  gulp.start('build')
+gulp.task("default", ["clean"], function() {
+    gulp.start('build')
 });
-
-
-
-
-
-
-
-
-
-
